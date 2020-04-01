58e3c9910b98e749f15024024b22e396264e930b
unknown <ascanzen@gmail.com>
2020-04-01 22:11:29 +0800

    add window plugin

--- window_utils/example/windows/flutter/generated_plugin_registrant.cc
+++ window_utils/example/windows/flutter/generated_plugin_registrant.cc
@@ -4,6 +4,9 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <window_utils_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  WindowUtilsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("WindowUtilsPlugin"));
 }
